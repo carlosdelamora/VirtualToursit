54b1450d7c68377d361c3af624aa0693b329bb45
Carlos De la Mora <carlosdelamorac@gmail.com>
2016-11-16 09:16:52 -0700

    First Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Carlos De la mora on 11/16/16.
+//  Copyright © 2016 Carlos De la mora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+  
+
+
+}
+
