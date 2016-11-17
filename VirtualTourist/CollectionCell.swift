b679f26b3711026976600241d2fc85257450ce97
Carlos De la Mora <carlosdelamorac@gmail.com>
2016-11-17 15:18:05 -0700

    Tap the pin to present the CollectionView

---
+++ VirtualTourist/CollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionCell.swift
+//  VirtualTourist
+//
+//  Created by Carlos De la mora on 11/17/16.
+//  Copyright © 2016 Carlos De la mora. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell{
+    
+    
+    
+}
---
+++ VirtualTourist/CollectionViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionViewController.swift
+//  VirtualTourist
+//
+//  Created by Carlos De la mora on 11/17/16.
+//  Copyright © 2016 Carlos De la mora. All rights reserved.
+//
+
+//import Foundation
+import MapKit
+import UIKit
+
+
+class CollectionViewController: UIViewController{
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+}
+
+
+extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1//placeHolder
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
+
